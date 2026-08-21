6a1aa4f2a353ce2e60c32d3d7faadaca371424d3
akaize <sahsa61842@gmail.com>
2026-08-21 05:57:10 +0300

    bubebe

--- common/src/main/java/v/akfz/cobe/test/CobeRegistries.java
+++ common/src/main/java/v/akfz/cobe/test/CobeRegistries.java
@@ -4,7 +4,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import v.akfz.aslib.AsLib;
 import v.akfz.aslib.annotation.RegisterModule;
-import v.akfz.aslib.initializer.generator.GenerateRegistries;
+import v.akfz.aslib.initializer.generator.GenerateRegistries;import v.akfz.aslib.registry.RegistryHelper;
 
 @GenerateRegistries(modId = "cobe")
 public class CobeRegistries {
@@ -13,7 +13,5 @@ public class CobeRegistries {
     }
 
     @RegisterModule(id = "cobe:testentity")
-    public static EntityType<TestEntity> TEST_ENTITY = EntityType.Builder.of(TestEntity::new, MobCategory.MISC)
-            .sized(1F, 1F)
-            .build("test_entity");
+    public static EntityType<TestEntity> TEST_ENTITY = RegistryHelper.createEntity(TestEntity::new,MobCategory.MISC,1f,1f,"cobe:testentity");
 }
