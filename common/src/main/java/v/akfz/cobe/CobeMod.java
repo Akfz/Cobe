a9bf4de7dd44dbd4437c060a51cb2515e39d53e1
akaize <sahsa61842@gmail.com>
2026-08-31 17:27:17 +0300

    bubebe

--- common/src/main/java/v/akfz/cobe/CobeMod.java
+++ common/src/main/java/v/akfz/cobe/CobeMod.java
@@ -6,7 +6,6 @@ import v.akfz.aslib.resourcepack.configpack.ConfigPack;
 import v.akfz.aslib.resourcepack.configpack.ConfigPackRegistry;
 import v.akfz.cobe.configpack.CobeCFGPack;
 import v.akfz.cobe.event.listener.TickListener;
-import v.akfz.cobe.test.RegisterEntityRendererListenerTEST;
 import v.akfz.db.generator.GenerateInitializer;
 
 @GenerateInitializer(modId = "cobe")
@@ -14,7 +13,7 @@ public class CobeMod {
     public void init() {
         AsLib.EVENT_BUS.register(new TickListener());
         ConfigPackRegistry.register("cobe", (path, data) -> new CobeCFGPack(data.name, path, data.id));
-        AsLib.EVENT_BUS.register(new RegisterEntityRendererListenerTEST());
+        //AsLib.EVENT_BUS.register(new RegisterEntityRendererListenerTEST());
         ConfigPack.Init();
 
         ModAssetsRegistrar.registerModAssets("cobe");
