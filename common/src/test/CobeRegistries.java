da5a39c6ed90271c1ceb947a11ffbdcfa69e9235
akaize <sahsa61842@gmail.com>
2026-08-31 17:24:36 +0300

    bubebe

--- common/src/test/CobeRegistries.java
+++ common/src/test/CobeRegistries.java
@@ -7,7 +7,7 @@ import v.akfz.aslib.annotation.RegisterModule;
 import v.akfz.aslib.initializer.generator.GenerateRegistries;
 import v.akfz.aslib.registry.RegistryHelper;
 
-//@GenerateRegistries(modId = "cobe")
+@GenerateRegistries(modId = "cobe")
 public class CobeRegistries {
     @RegisterModule(id = "cobe:testentity")
     public static EntityType<TestEntity> TEST_ENTITY = RegistryHelper.createEntity(TestEntity::new,MobCategory.MISC,1f,1f,"cobe:testentity");
